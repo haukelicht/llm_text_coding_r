266afd6b128bbc7a6a41d7e1b06befb35be47eb9
Hauke Licht <hauke.licht@t-online.de>
2024-06-07 09:51:46 +0200

    updated renv setup script

--- setup/setup_renv.R
+++ setup/setup_renv.R
@@ -1,17 +1,27 @@
-if (!"renv" %in% rownames(installed.packages()))
-  install.packages("renv")
+# list installed packages
+installed_pkgs <- rownames(installed.packages())
 
+# install required version of {renv} (if needed)
+renv_version <- "1.0.7"
+if (!"renv" %in% installed_pkgs) {
+  if (!"remotes" %in% installed_pkgs)
+    install.packages("remotes")
+  remotes::install_version("renv", version = renv_version)
+} else if (packageVersion("renv") != renv_version) {
+  remotes::install_version("renv", version = renv_version)
+}
+
+# init the {renv} environment
+renv::init(bare = TRUE, restart = TRUE)
+
+# install the required packages
 lock_file <- "renv.lock"
 if (file.exists(lock_file)) {
-  renv::init(bare = TRUE, restart = FALSE)
   renv::restore(lockfile = lock_file, prompt = FALSE)
 } else {
-  renv::init(bare = TRUE, restart = FALSE)
-  
   pkgs <- readLines(file.path("setup", "requirements.txt"))
   renv::install(pkgs)
-  
-  renv::snapshot()
-} 
+  renv::snapshot(lockfile = lock_file)
+}
 
 
